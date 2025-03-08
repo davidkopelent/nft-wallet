33e3be9f6501f57132a1bbbfa3817a60457b972e
David Kopelent <davidekkopelent@gmail.com>
2025-03-08 01:16:03 +0100

    Add: Nextjs toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import './globals.css';
 import { ThemeProvider } from '@/context/ThemeContext';
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata = {
   title: 'Cardano Wallet Explorer',
@@ -15,6 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="inter">
+        <NextTopLoader 
+          color="#3B82F6" // Blue color matching your theme
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+        />
         <ThemeProvider>
           <Navbar />
           <main className="min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-gray-900 dark:to-gray-800 py-8">
